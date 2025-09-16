3377dbb6cb79b92acf632c592b76b1180a20414e
ukcheruku8 <ukcheruku8@gmail.com>
2025-09-16 15:32:09 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Testimonials from './components/Testimonials';
+import Pricing from './components/Pricing';
+import FAQ from './components/FAQ';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Testimonials />
+      <Pricing />
+      <FAQ />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
